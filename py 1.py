bcd1a38658ce7c3f3f8663fa5f7fd33ea7c00020
SAFA THASNIM <safathasnim269@gmail.com>
2025-07-29 10:18:13 +0530

    S1

---
+++ py 1.py
@@ -0,0 +1,15 @@
+print("ARITHMETIC OPERATIONS")
+print("----------------------")
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second:"))
+print("RESULT")
+c=a+b
+print("ADDITION:",c)
+c=a-b
+print("SUBTRACTION:",c)
+c=a*b
+print("MULTIPLICATION:",c)
+c=a/b
+print("DIVISION:",c)
+c=a%b
+print("MODULAS:",c)
